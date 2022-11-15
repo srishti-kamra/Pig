b0416204e88d36d2379dd376ad62a8aee28489c7
Srishti Kamra <srishtikamra07@gmail.com>
2022-11-15 10:09:58 -0600

    Initial Commit

---
+++ Pig/PigApp.swift
@@ -0,0 +1,17 @@
+//
+//  PigApp.swift
+//  Pig
+//
+//  Created by Srishti Kamra  on 11/15/22.
+//
+
+import SwiftUI
+
+@main
+struct PigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
